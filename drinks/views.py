6c2b1e5f77487e5bb46fe095195cda7516e9a38c
QuantumBen <omosabenedict@gmail.com>
2025-09-05 04:59:32 -0700

    Implement POST request

--- drinks/views.py
+++ drinks/views.py
@@ -5,7 +5,19 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET', 'POST'])
 def drink_list(request):
-    drinks = Drink.objects.all()
-    serializer = DrinkSerializer(drinks, many=True)
-    return JsonResponse({"drinks": serializer.data}, safe=False)
+
+    if request.method == 'GET':
+        drinks = Drink.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
+        return JsonResponse({"drinks": serializer.data}, safe=False)
+
+    if request.method == 'POST':
+        data = request.data
+        serializer = DrinkSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
 
