c013058787e26af665262b676d4744b85061a256
QuantumBen <omosabenedict@gmail.com>
2025-09-04 22:51:52 -0700

    Change object on the admin site to something readable

--- drinks/models.py
+++ drinks/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Drink(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.name + " - " + self.description
